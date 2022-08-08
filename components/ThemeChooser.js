42f4349c54e855a968e892872dcbb6414e385bac
Bmcgaughan <87346945+Bmcgaughan@users.noreply.github.com>
2022-08-08 13:56:39 -0400

    snapback

--- components/ThemeChooser.js
+++ components/ThemeChooser.js
@@ -35,6 +35,22 @@ class ThemeChooser extends Component {
       },
       onPanResponderTerminationRequest: () => true,
       onResponderSingleTapConfirmed: (evt, gestureState) => {},
+      onResponderRelease: (evt, gestureState) => {
+        const { x, y } = this.state;
+        console.log(y);
+        if (y._value < 200) {
+          this.state.bounceValue.setValue(550 + y._value);
+          this.state.menuBounce.setValue(0 + y._value);
+          this.handleSnapTransition(550, this.state.bounceValue);
+          this.handleSnapTransition(0, this.state.menuBounce);
+          y.setValue(0);
+        }
+
+        // }
+        // console.log(y);
+        // console.log(y);
+        // this.setState({ y: new Animated.Value(0) });
+      },
     });
   }
 
@@ -47,6 +63,7 @@ class ThemeChooser extends Component {
 
   pan = (gestureState) => {
     const { x, y } = this.state;
+
     const maxX = 0;
     const minX = 0;
     const maxY = 5000;
@@ -69,21 +86,21 @@ class ThemeChooser extends Component {
       newY = maxY;
     }
 
-    if (newY > 120) {
-      this.handleMenuTransition('down');
-
-      this.handleDragTransition('down');
-    }
     x.setValue(newX);
     y.setValue(newY);
+
+    if (newY > 200) {
+      this.handleMenuTransition('down');
+      this.handleDragTransition('down');
+    }
   };
 
   handleDragTransition(direction) {
     Animated.spring(this.state.bounceValue, {
-      toValue: direction === 'down' ? 1000 : 600,
+      toValue: direction === 'down' ? 1000 : 550,
       velocity: 3,
-      tension: 2,
-      friction: 8,
+      tension: 0,
+      friction: 15,
       useNativeDriver: false,
     }).start(({ finished }) => {
       if (finished && direction === 'down') {
@@ -97,8 +114,18 @@ class ThemeChooser extends Component {
     Animated.spring(this.state.menuBounce, {
       toValue: direction === 'down' ? 1000 : 0,
       velocity: 3,
-      tension: 2,
-      friction: 8,
+      tension: 4,
+      friction: 15,
+      useNativeDriver: false,
+    }).start();
+  }
+
+  handleSnapTransition(offset, source) {
+    Animated.spring(source, {
+      toValue: offset,
+      velocity: 1,
+      tension: 4,
+      friction: 3,
       useNativeDriver: false,
     }).start();
   }
@@ -111,9 +138,6 @@ class ThemeChooser extends Component {
   render() {
     const { x, y, bounceValue, menuBounce, darkMode } = this.state;
     const imageStyle = { left: x, top: y };
-
-    const menuYOffset = new Animated.Value(y._value + 600);
-
     const menuSyle = { left: x, top: y };
 
     return (
@@ -223,30 +247,32 @@ class ThemeChooser extends Component {
 
 const styles = StyleSheet.create({
   dragContainer: {
+    position: 'absolute',
     width: '100%',
-    height: 20,
+    height: 50,
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
+    zIndex: 1000,
+    elevation: 1000,
   },
   dragTop: {
     width: '100%',
+    height: 75,
     justifyContent: 'flex-start',
     alignItems: 'center',
   },
   lineImage: {
     paddingTop: 20,
-    width: 200,
+    width: 100,
     height: 1,
-  },
-  draggable: {
-    height: 50,
-    width: 50,
   },
   menuHeader: {
     top: 600,
-    height: 50,
+    height: 75,
     width: '100%',
     paddingTop: 10,
+    zIndex: 1000,
+    elevation: 1000,
   },
   menuText: {
     paddingLeft: 20,
@@ -257,12 +283,14 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'space-between',
     width: '100%',
-    height: 230,
+    height: 270,
     alignItems: 'center',
     paddingLeft: 40,
     paddingRight: 40,
     paddingBottom: 50,
     paddingTop: 20,
+    zIndex: 1000,
+    elevation: 1000,
   },
   menuRow: {
     flexDirection: 'row',
